e1027170a9af6ba683f40f710b645885ad2da0f8
RaeedA <raeedazom@icloud.com>
2020-10-13 21:37:41 -0700

    Testing

---
+++ RaeedAzom.kts
@@ -0,0 +1,35 @@
+/*
+Print out your name onto the console.
+
+e.g. your program should print "Andy Min".
+ */
+fun level1() {
+    print("Raeed Azom");
+}
+
+/*
+Print out how many letters your name has.
+
+DON'T hardcode a number and just print "8" for example.
+You should set your name as a variable, and then print out how many letters
+that variable has.
+ */
+fun level2() {
+    print("Raeed Azom".length-1)
+}
+
+/*
+Prompt the user to enter their some text, and then print out only the UPPERCASE
+letters.
+
+e.g. Entering "tHis iS a rANdoM meSSagE" should print out "HSANMSSE"
+ */
+fun level3() {
+    // Your code here
+}
+
+
+
+level1()
+level2()
+level3()
