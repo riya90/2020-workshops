2a7e7fe6faa13733ced9161de1b46e21b25e61e1
CindyL688 <71861322+CindyL688@users.noreply.github.com>
2020-10-16 17:24:06 -0700

    some (#17)

---
+++ CindyLiang.kts
@@ -0,0 +1,46 @@
+/*
+Print out your name onto the console.
+
+e.g. your program should print "Andy Min".
+ */
+fun level1() {
+    println("Cindy Liang")
+}
+
+/*
+Print out how many letters your name has.
+
+DON'T hardcode a number and just print "8" for example.
+You should set your name as a variable, and then print out how many letters
+that variable has.
+ */
+fun level2() {
+    val name = "Cindy Liang"
+    println(name.length)
+
+}
+
+/*
+Prompt the user to enter their some text, and then print out only the UPPERCASE
+letters.
+
+e.g. Entering "tHis iS a rANdoM meSSagE" should print out "HSANMSSE"
+ */
+fun level3() {
+    println("enter any text: ")
+    val input = readLine()!!
+
+    for (character in input){
+        if(character.isUpperCase()){
+            print(character)
+        }
+    }
+
+
+}
+
+
+
+    level1()
+    level2()
+    level3()
