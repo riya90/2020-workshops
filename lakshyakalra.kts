eec689fc92729ef54263489a80852b94d9ea4467
Lakshya377 <72583307+Lakshya377@users.noreply.github.com>
2020-10-16 17:19:49 -0700

    Add lakshyakalra.kt (#9)
    Co-authored-by: Lakshya Kalra <lakshya@Lakshyas-MacBook-Pro.local>

---
+++ lakshyakalra.kts
@@ -0,0 +1,37 @@
+/*
+Print out your name onto the console.
+
+e.g. your program should print "Andy Min".
+ */
+fun level1() {
+    System.out.println ("Lakshya Kalra");
+
+}
+
+/*
+Print out how many letters your name has.
+
+DON'T hardcode a number and just print "8" for example.
+You should set your name as a variable, and then print out how many letters
+that variable has.
+ */
+fun level2() {
+    val myname = "lakshya Kalra"
+    println(myname.length)
+}
+
+/*
+Prompt the user to enter their some text, and then print out only the UPPERCASE
+letters.
+
+e.g. Entering "tHis iS a rANdoM meSSagE" should print out "HSANMSSE"
+ */
+fun level3() {
+    // Your code here
+}
+
+
+
+level1()
+level2()
+level3()
