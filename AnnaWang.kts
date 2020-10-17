70afbdd2c00b22f35f5dd9f6a4b23c5dc4ada333
andragonfruit <71798196+andragonfruit@users.noreply.github.com>
2020-10-16 17:19:40 -0700

    Added AnnaWang.kts (#8)
    Co-authored-by: Anna Wang <AnnaWang@Annas-Air.attlocal.net>

---
+++ AnnaWang.kts
@@ -0,0 +1,41 @@
+/*
+Print out your name onto the console.
+
+e.g. your program should print "Andy Min".
+ */
+fun level1() {
+    println("Anna Wang")
+}
+
+/*
+Print out how many letters your name has.
+
+DON'T hardcode a number and just print "8" for example.
+You should set your name as a variable, and then print out how many letters
+that variable has.
+ */
+fun level2() {
+    val myName = "Anna Wang"
+    println(myName.length)
+}
+
+/*
+Prompt the user to enter their some text, and then print out only the UPPERCASE
+letters.
+
+e.g. Entering "tHis iS a rANdoM meSSagE" should print out "HSANMSSE"
+ */
+fun level3() {
+    val input = readLine()!!
+    for (character in input){
+        if (character.isUppercase()){
+            print(character)
+        }
+    }
+}
+
+
+
+level1()
+level2()
+level3()
