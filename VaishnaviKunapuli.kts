a2773c7948fac6ec132a306b7eb661bb3ca5b5e0
kunapulivaishnavi <47906380+kunapulivaishnavi@users.noreply.github.com>
2020-10-16 17:19:58 -0700

    Added VaishnaviKunapuli.kt (#16)

---
+++ VaishnaviKunapuli.kts
@@ -0,0 +1,45 @@
+/*
+Print out your name onto the console.
+
+e.g. your program should print "Andy Min".
+ */
+fun level1() {
+    println("Andy Min")
+}
+
+/*
+Print out how many letters your name has.
+
+DON'T hardcode a number and just print "8" for example.
+You should set your name as a variable, and then print out how many letters
+that variable has.
+ */
+fun level2() {
+    val name = "Vaishnavi"
+    println(name.length)
+
+}
+
+/*
+Prompt the user to enter their some text, and then print out only the UPPERCASE
+letters.
+
+e.g. Entering "tHis iS a rANdoM meSSagE" should print out "HSANMSSE"
+ */
+
+
+fun level3() {
+    val input = readLine()!!
+    for(character in input) {
+        if(character.isUpperCase()){
+            println(character)
+        }
+
+    }
+}
+
+
+
+level1()
+level2()
+level3()
