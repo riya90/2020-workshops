77caf220a888a4cd7951d7db25ebdb0173d2e516
Andrew (Andy) Min <me@andrewmin.info>
2020-10-16 17:30:06 -0700

    Revert2

--- _TEMPLATE.kts
+++ _TEMPLATE.kts
@@ -1,5 +1,6 @@
 /*
 Print out your name onto the console.
+
 e.g. your program should print "Andy Min".
  */
 fun level1() {
@@ -8,6 +9,7 @@ fun level1() {
 
 /*
 Print out how many letters your name has.
+
 DON'T hardcode a number and just print "8" for example.
 You should set your name as a variable, and then print out how many letters
 that variable has.
@@ -19,6 +21,7 @@ fun level2() {
 /*
 Prompt the user to enter their some text, and then print out only the UPPERCASE
 letters.
+
 e.g. Entering "tHis iS a rANdoM meSSagE" should print out "HSANMSSE"
  */
 fun level3() {
