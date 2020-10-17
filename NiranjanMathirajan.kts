9203e684002dcaa844d56414e307133636bc3b94
Neragin <56417211+Neragin@users.noreply.github.com>
2020-10-16 17:21:52 -0700

    added NiranjanMathirajan.kts (#21)

---
+++ NiranjanMathirajan.kts
@@ -0,0 +1,39 @@
+/*
+Print out your name onto the console.
+
+e.g. your program should print "Andy Min".
+ */
+fun level1() {
+    // Your code here1
+    println("Niranjan Mathirajan")
+}
+
+/*
+Print out how many letters your name has.
+
+DON'T hardcode a number and just print "8" for example.
+You should set your name as a variable, and then print out how many letters
+that variable has.
+ */
+fun level2() {
+    // Your code here
+    val name = "Niranjan Mathirajan"
+    println(name.length)
+}
+
+/*
+Prompt the user to enter their some text, and then print out only the UPPERCASE
+letters.
+
+e.g. Entering "tHis iS a rANdoM meSSagE" should print out "HSANMSSE"
+ */
+fun level3() {
+    // Your code here
+    for( charectar in input)
+}
+
+
+
+level1()
+level2()
+level3()
